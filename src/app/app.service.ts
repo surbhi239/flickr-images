c8a23247916ef4e3663c5b43f7b30b54101ca72f
Surbhi <surbhigirdhani@gmail.com>
2020-07-26 22:00:07 +0530

    image URL

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
-const URL = environment.imageURL;
+const URL = 'https://www.flickr.com/services/rest/?method=flickr.people.getPhotos&api_key=130c354ac8146e0973960f42a74a9cef&user_id=189430311%40N05&format=json&nojsoncallback=1';
 
 @Injectable({
   providedIn: 'root'
